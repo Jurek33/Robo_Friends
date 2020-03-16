e8ac35ad26b880694bce401f4e5c1ce594dda5e2
Yury Hrytsuk <jerzy.gricuk@yurys-air.attlocal.net>
2020-03-16 18:22:34 -0400

    Updates

---
+++ precache-manifest.f3779a93a6746d353246cb6bba070fd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19c92438780c37bda742c7232ee38681",
+    "url": "/Robo_Friends/index.html"
+  },
+  {
+    "revision": "12a7705308b95f519a46",
+    "url": "/Robo_Friends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "046b477399c18e8f4396",
+    "url": "/Robo_Friends/static/css/main.e710edf0.chunk.css"
+  },
+  {
+    "revision": "12a7705308b95f519a46",
+    "url": "/Robo_Friends/static/js/2.5a2da438.chunk.js"
+  },
+  {
+    "revision": "046b477399c18e8f4396",
+    "url": "/Robo_Friends/static/js/main.5b3607b0.chunk.js"
+  },
+  {
+    "revision": "2e8e2b64b965595c2670",
+    "url": "/Robo_Friends/static/js/runtime-main.36ae5c1e.js"
+  }
+]);
